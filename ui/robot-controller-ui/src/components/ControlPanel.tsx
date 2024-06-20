9ca6f5f72611519277b63078ac93ce639d3d9564
Abel Mendoza <abelxmendoza@gmail.com>
2024-06-19 21:50:26 -0700

    added backend server and got the requests to work

--- ui/robot-controller-ui/src/components/ControlPanel.tsx
+++ ui/robot-controller-ui/src/components/ControlPanel.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 
-const ControlPanel: React.FC = () => {
+interface ControlPanelProps {
+  onUp: () => void;
+  onDown: () => void;
+  onLeft: () => void;
+  onRight: () => void;
+}
+
+const ControlPanel: React.FC<ControlPanelProps> = ({ onUp, onDown, onLeft, onRight }) => {
   return (
-    <div className="grid grid-cols-3 gap-4 w-32">
-      <button className="bg-blue-500 text-white p-2">Up</button>
-      <button className="bg-blue-500 text-white p-2">Left</button>
-      <button className="bg-blue-500 text-white p-2">Right</button>
-      <button className="bg-blue-500 text-white p-2">Down</button>
+    <div className="grid grid-rows-3 grid-cols-3 gap-2 w-32 h-32">
+      <button className="bg-blue-500 text-white p-2 col-start-2" onClick={onUp}>Up</button>
+      <button className="bg-blue-500 text-white p-2 row-start-2 col-start-1" onClick={onLeft}>Left</button>
+      <button className="bg-blue-500 text-white p-2 row-start-2 col-start-3" onClick={onRight}>Right</button>
+      <button className="bg-blue-500 text-white p-2 row-start-3 col-start-2" onClick={onDown}>Down</button>
     </div>
   );
 };
