8a174f5dc13f848d55129c66543b57ed8aa689fb
Abel Mendoza <abelxmendoza@gmail.com>
2024-06-21 16:06:58 -0700

    added video feed ip connection with tailscale

--- ui/robot-controller-ui/src/components/VideoFeed.tsx
+++ ui/robot-controller-ui/src/components/VideoFeed.tsx
@@ -1,9 +1,10 @@
+// File: /Omega-Code/ui/robot-controller-ui/src/components/VideoFeed.tsx
 import React from 'react';
 
 const VideoFeed: React.FC = () => {
   return (
     <div className="w-2/5 bg-gray-200 flex items-center justify-center" style={{ height: 'calc(60vw * 0.6)' }}>
-      <span>Video Feed Placeholder</span>
+      <img src="http://100.68.201.128:5000/video_feed" alt="Video Feed" className="w-full h-full object-cover" />
     </div>
   );
 };
