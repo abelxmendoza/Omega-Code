8d4b7d597b0edd342efe124313156c29b7aa9ffb
Abel Mendoza <abelxmendoza@gmail.com>
2024-06-30 19:09:50 -0700

    added gps location

--- ui/robot-controller-ui/src/components/VideoFeed.tsx
+++ ui/robot-controller-ui/src/components/VideoFeed.tsx
@@ -5,17 +5,38 @@ This component displays a video feed from a specified URL.
 It renders the video stream inside a div container with predefined styles for width and height.
 */
 
-import React from 'react';
+import React, { useState } from 'react';
+import GpsLocation from './GpsLocation';
 
 const VideoFeed: React.FC = () => {
+  const [showGps, setShowGps] = useState(false);
+
+  const toggleView = () => {
+    setShowGps(!showGps);
+  };
+
   return (
-    <div className="w-2/5 bg-gray-200 flex items-center justify-center" style={{ height: 'calc(60vw * 0.6)' }}>
-      {/* Display the video feed from the specified URL */}
-      <img 
-        src="http://100.68.201.128:5000/video_feed" 
-        alt="Video Feed" 
-        className="w-full h-full object-cover" 
-      />
+    <div className="relative w-2/5 bg-gray-200 flex items-center justify-center" style={{ height: 'calc(60vw * 0.6)' }}>
+      {showGps ? (
+        <div className="absolute top-0 right-0 w-full h-full">
+          <GpsLocation size="large" onSwitchToVideo={toggleView} />
+        </div>
+      ) : (
+        <>
+          <img 
+            src="http://100.68.201.128:5000/video_feed" 
+            alt="Video Feed" 
+            className="w-full h-full object-cover" 
+          />
+          <div 
+            className="absolute top-2 right-2 w-24 h-24 cursor-pointer"
+            onClick={toggleView}
+            title="Click to enlarge"
+          >
+            <GpsLocation size="small" onSwitchToVideo={toggleView} />
+          </div>
+        </>
+      )}
     </div>
   );
 };
