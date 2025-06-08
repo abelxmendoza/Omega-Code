340705b0e8a9d77c3552ab1380dcf7579b569da0
Abel Mendoza <42751205+abelxmendoza@users.noreply.github.com>
2025-06-07 20:28:17 -0700

    Restore led_control wrapper and add package init files

---
+++ servers/robot-controller-backend/utils/led_control.py
@@ -0,0 +1,11 @@
+from controllers.lighting.led_control import LedControl
+
+class Led(LedControl):
+    """Backward-compatible wrapper around :class:`LedControl`."""
+
+    # Alias methods to maintain legacy API
+    colorWipe = LedControl.color_wipe
+    theaterChase = LedControl.theater_chase
+    setLed = LedControl.set_led
+    LED_TYPR = LedControl._convert_color
+
