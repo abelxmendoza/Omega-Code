1de2046d4ff26a2386848972304643c15872fe98
Abel Mendoza <42751205+abelxmendoza@users.noreply.github.com>
2025-06-07 21:41:03 -0700

    Remove compiled binaries and update gitignore

--- servers/robot-controller-backend/rust_module/target/release/deps/rust_module-7445da1925ac1626.d
+++
@@ -1,7 +0,0 @@
-/Users/abel_elreaper/Desktop/Omega-Code/servers/robot-controller-backend/rust_module/target/release/deps/rust_module-7445da1925ac1626.rmeta: src/lib.rs
-
-/Users/abel_elreaper/Desktop/Omega-Code/servers/robot-controller-backend/rust_module/target/release/deps/librust_module-7445da1925ac1626.rlib: src/lib.rs
-
-/Users/abel_elreaper/Desktop/Omega-Code/servers/robot-controller-backend/rust_module/target/release/deps/rust_module-7445da1925ac1626.d: src/lib.rs
-
-src/lib.rs:
